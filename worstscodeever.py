e23fd923a66ccec3ddd199de0ef235e2154b0d14
unknown <keven_jkc@hotmail.com>
2022-02-11 17:07:17 -0300

    close

--- worstscodeever.py
+++ worstscodeever.py
@@ -17,15 +17,6 @@ for i in range(size):
     password += choice(values)
 print(f"A senha da vez é {password}")
 
-# for i in range(1,10+1):
-#     time.sleep(1)
-#     size = 5
-#     values = string.ascii_uppercase + string.digits
-#     attempt = ''
-#     for i in range(size):
-#         attempt += choice(values)
-#         print(attempt)
-
 time.sleep(3)
 size = 5
 values = string.ascii_uppercase + string.digits
@@ -47,28 +38,27 @@ while attempt != password:
         print('Chega fi, cabô')
         break
 
-
 ##Repetition
-if max_attempts == 9:
-    max_attempts = 0
-    time.sleep(3)
-    size = 5
-    values = string.ascii_uppercase + string.digits
-    password = ''
-    for i in range(size):
-        password += choice(values)
-print(f"A senha da vez é {password}")
-
-while attempt != password:
-    attempt = ''
-    time.sleep(1)
-    for elements in range(size):
-        attempt += choice(values)
-    print(attempt)
-    max_attempts += 1
-    if attempt == password or max_attempts == 9:
-        print('Chega fi, cabô')
-        break
+while True:
+    if max_attempts == 9:
+        max_attempts = 0
+        time.sleep(3)
+        size = 5
+        values = string.ascii_uppercase + string.digits
+        password = ''
+        for i in range(size):
+            password += choice(values)
+    print(f"A senha da vez é {password}")
+    while attempt != password:
+        attempt = ''
+        time.sleep(1)
+        for elements in range(size):
+            attempt += choice(values)
+        print(attempt)
+        max_attempts += 1
+        if attempt == password or max_attempts == 9:
+            print('Chega fi, cabô')
+            break
 
 
 
