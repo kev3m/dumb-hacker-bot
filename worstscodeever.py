32d90b52ccc327967382e0f4a9352c8e8c8f6058
unknown <keven_jkc@hotmail.com>
2022-02-11 14:18:50 -0300

    simplified number generator + timer

--- worstscodeever.py
+++ worstscodeever.py
@@ -3,12 +3,26 @@
 
 
 import string
-import random
+from random import random, choice
+import time
 
+# string.ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# string.digits = "0123456789"
+
+time.sleep(3)
+
+size = 5
+values = string.ascii_uppercase + string.digits
+password = ''
+for i in range(size):
+    password += choice(values)
+
+print(password)
+    
 #def function = define
 
-def combination_generator(
-size=5, 
-chars=string.ascii_uppercase + string.digits):
- return ''.join(random.choice(chars) for _ in range(size))
-print(combination_generator()) # M4ICUV
+# def combination_generator(
+# size=5, 
+# chars=string.ascii_uppercase + string.digits):
+#  return ''.join(random.choice(chars) for _ in range(size))
+# print(combination_generator()) # M4ICUV
