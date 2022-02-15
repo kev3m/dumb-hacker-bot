2da664d1efacfc40ad19142b48f2a9d1949876c1
unknown <keven_jkc@hotmail.com>
2022-02-15 16:19:52 -0300

    live version

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from worstcode import password
+
+
+# app = Flask(__name__)
+
+
+
+
+# @app.route("/")
+# def index():
+#     nome_da_variavel = "Zap"
+#     return render_template('index.html', variavel=nome_da_variavel)
+
+
+
+# # @app.route('/password')
+# # def showPassword():
+# #     return password()
+
+
+# app.run(debug=True)
